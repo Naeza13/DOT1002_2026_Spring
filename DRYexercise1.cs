6cde5ef9774a4b2a7e8aa1db54bf017318e412fd
Halil Uçar <halilibrahimucar@posta.mu.edu.tr>
2026-03-09 12:17:05 +0300

    Revert "Exercise"
    This reverts commit abbef6e43821c3e41dadde218216c20eaaff1e70.

--- DRYexercise1.cs
+++
@@ -1,9 +0,0 @@
-string odun = "";
-
-string Ekle(string maden, int miktar)
-{
-    return maden  =miktar.ToString();
-} 
-
-odun = Ekle(odun, 25);
-Console.Write(odun);
--- DRYexercise3.cs
+++
@@ -1,7 +0,0 @@
-    int health= 100;
-
-    void Hasar(string dmgtype,int miktar)
-    {
-        health-=miktar;
-        if (health<0) health=0;
-    }
